41913af4a09b48313473577e37f525be536f02df
Rhonda <rtinkham0408@gmail.com>
2018-12-27 00:37:56 -0500

    Finished backend

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+// Body Parser middleware
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
 
 // DB config
 const db = require('./config/keys_dev').mongoURI;
@@ -24,7 +32,11 @@ mongo.then(() => {
 //   .connect(db)
 //   .then(() => console.log('MongoDB Connected!'));
 
-app.get('/', (req, res) => res.send('Hello!!!'));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Use Routes
 app.use('/api/users', users);
